def523dc74ce968d0a78a156a16704a27ce44483
jun <jun@gmail.com>
2020-05-24 11:38:13 +1200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+/* example
+  const HomePage = props=>{
+    return(
+      <div>
+      <button onclick={()=> props.history.push('/topics')}Topics> </button>
+      <h1>Home page</h1>
+      </div>
+    )
+  }
+*/
+/* example
+  const TopicsList = props=>{
+    return(
+      <div>
+      <h1>Topic list</h1>
+      <link to={`${props.match.url}/13`}>to topic 13<link>
+      <link to={`${props.match.url}/15`}>to topic 15<link>
+      <link to={`${props.match.url}/16`}>to topic 16<link>
+      </div>
+    )
+  }
+*/
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        {' '}
+        {/* switch will match '/' first then /hats */}
+        {/* example
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+        <Route path='/hats/:hatsID' component={HatsDetail} />
+        */}
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
